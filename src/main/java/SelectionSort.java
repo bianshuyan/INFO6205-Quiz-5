40438423ae0509cf958c7c2919a93654e9912982
bianshuyan <1255581891@qq.com>
2025-03-20 13:08:07 -0400

    Update SelectionSort.java

--- src/main/java/SelectionSort.java
+++ src/main/java/SelectionSort.java
@@ -1,8 +1,16 @@
 class SelectionSort {
     public static void selectionSort(int[] arr) {
         int n = arr.length;
-
-       // STUDENT TODO
+        // STUDENT TODO
+        for (int i = 0; i < n; i++) {
+            int m = i;
+            for (int j = i; j < n; j++) {
+                if (arr[j] < arr[m]) m = j;
+            }
+            int temp = arr[i];
+            arr[i] = arr[m];
+            arr[m] = temp;
+        }
     }
 
     public static void main(String[] args) {
